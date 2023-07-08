b00703f0f36eae87bb3692777eeb44c9fe56289f
محمد حسین باباعلی <babaali.m@avadb.ir>
2023-07-08 17:30:03 +0330

    data added
    asdf

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from blog.models import Post
 
 def blog_views (request):
     return render (request,'blog/blog-home.html')
@@ -9,3 +9,9 @@ def blog_views (request):
 
 def blog_single (request):
     return render (request,'blog/blog-single.html')
+
+
+def test (request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render (request,'test.html',context)
