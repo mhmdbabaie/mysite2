5352dc143f78bd00d518840c8b8f5e4fcccdcde7
mhmdbabaie <110888361+mhmdbabaie@users.noreply.github.com>
2023-07-10 16:41:04 +0330

    url parameters
    msg

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
 def blog_views (request):
@@ -13,6 +13,8 @@ def blog_single (request):
     return render (request,'blog/blog-single.html')
 
 
-def test (request,name):
-    context = {'name': name}
+def test (request,pid):
+    #post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid)
+    context = {'post': post}
     return render (request,'test.html',context)
