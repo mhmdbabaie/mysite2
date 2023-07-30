baabf7745ae78be07596c8259fb48c300d951dda
mhmdbabaie <110888361+mhmdbabaie@users.noreply.github.com>
2023-07-30 17:04:21 +0330

    snippet
    خلاصه سازی content

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 # Register your models here.
 
 
@@ -14,3 +14,4 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ['title','content']
 
 admin.site.register(Post,PostAdmin) 
+admin.site.register(Category)                                                                                                                                                           
